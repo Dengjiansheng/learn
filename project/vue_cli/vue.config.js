dcc14debd1fdea23818044d67f06466d9cdc5a72
d_js <d_js@foxmail.com>
2018-06-25 19:12:25 +0800

    联动

---
+++ project/vue_cli/vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    /*options...
+    * baseUrl
+    * outputDir
+    * assetsDir
+    * pages
+    * lintOnSave
+    * runtimeCompiler
+    * transpileDependencies
+    * productionSourceMap
+    * configureWebpack
+    * chainWebpack
+    * css.modules
+    * css.extract
+    * css.sourceMap
+    * css.loaderOptions
+    * devServer
+    * devServer.proxy
+    * parallel
+    * pwa
+    * pluginOptions
+    */
+   assetsDir: 'static/'
+}
