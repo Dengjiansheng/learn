8e955c9a539cf27ad8d88d46c106b7256e2bb1d6
d_js <d_js@foxmail.com>
2018-08-27 19:10:07 +0800

    my-app
    ng

--- project/my-app/src/main.ts
+++ project/my-app/src/main.ts
@@ -4,8 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
 if (environment.production) {
   enableProdMode();
+  // TODO 修改
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
